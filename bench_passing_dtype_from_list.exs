48e7e3a0e07dc5a65641c02d16182e69ce682c47
Philip Sampaio <philip.sampaio@gmail.com>
2024-06-12 15:58:42 -0300

    WIP: testing with lists of lists

--- bench_passing_dtype_from_list.exs
+++ bench_passing_dtype_from_list.exs
@@ -2,21 +2,42 @@ ints_generator = fn max_num, transform ->
   Enum.map(0..max_num, fn _ -> transform.(:rand.uniform(200_000)) end)
 end
 
+lists_of_lists_of = fn max_num, generator ->
+  Enum.map(0..max_num, fn _ -> generator.() end)
+end
+
+# inputs = %{
+#   "small list of ints" => {:date, ints_generator.(1_000, &Function.identity/1)},
+#   "medium list of ints" => {:date, ints_generator.(100_000, &Function.identity/1)},
+#   "big list of ints" => {:date, ints_generator.(1_000_000, &Function.identity/1)},
+#   "small list of dates w/ dtype" => {:date, ints_generator.(1_000, &Date.from_gregorian_days/1)},
+#   "medium list of dates w/ dtype" =>
+#     {:date, ints_generator.(100_000, &Date.from_gregorian_days/1)},
+#   "big list of dates w/ dtype" =>
+#     {:date, ints_generator.(1_000_000, &Date.from_gregorian_days/1)},
+#   "small list of dates without dtype" =>
+#     {nil, ints_generator.(1_000, &Date.from_gregorian_days/1)},
+#   "medium list of dates without dtype" =>
+#     {nil, ints_generator.(100_000, &Date.from_gregorian_days/1)},
+#   "big list of dates without dtype" =>
+#     {nil, ints_generator.(1_000_000, &Date.from_gregorian_days/1)}
+# }
 inputs = %{
-  "small list of ints" => {:date, ints_generator.(1_000, &Function.identity/1)},
-  "medium list of ints" => {:date, ints_generator.(100_000, &Function.identity/1)},
-  "big list of ints" => {:date, ints_generator.(1_000_000, &Function.identity/1)},
-  "small list of dates w/ dtype" => {:date, ints_generator.(1_000, &Date.from_gregorian_days/1)},
-  "medium list of dates w/ dtype" =>
-    {:date, ints_generator.(100_000, &Date.from_gregorian_days/1)},
-  "big list of dates w/ dtype" =>
-    {:date, ints_generator.(1_000_000, &Date.from_gregorian_days/1)},
-  "small list of dates without dtype" =>
-    {nil, ints_generator.(1_000, &Date.from_gregorian_days/1)},
-  "medium list of dates without dtype" =>
-    {nil, ints_generator.(100_000, &Date.from_gregorian_days/1)},
-  "big list of dates without dtype" =>
-    {nil, ints_generator.(1_000_000, &Date.from_gregorian_days/1)}
+  "list of lists of dates as ints" =>
+    {{:list, :date},
+     lists_of_lists_of.(1_000, fn ->
+       ints_generator.(1_000, &Function.identity/1)
+     end)},
+  "list of lists of dates as dates" =>
+    {{:list, :date},
+     lists_of_lists_of.(1_000, fn ->
+       ints_generator.(1_000, &Date.from_gregorian_days/1)
+     end)},
+  "list of lists of dates as dates without dtype" =>
+    {nil,
+     lists_of_lists_of.(1_000, fn ->
+       ints_generator.(1_000, &Date.from_gregorian_days/1)
+     end)}
 }
 
 Benchee.run(
